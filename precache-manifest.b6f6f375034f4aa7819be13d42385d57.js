00d6c2c4c320cf0eba76ad6302b1fb948da74fa9
zephinzer <dev@joeir.net>
2019-10-29 21:25:38 +0800

    Updates

---
+++ precache-manifest.b6f6f375034f4aa7819be13d42385d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de0a645661647940e77ea739010665f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b33c2a772d00ada83b6a",
+    "url": "/static/css/main.df67b0ba.chunk.css"
+  },
+  {
+    "revision": "ed6ab4298e2e455fd9ae",
+    "url": "/static/js/2.aa80af83.chunk.js"
+  },
+  {
+    "revision": "b33c2a772d00ada83b6a",
+    "url": "/static/js/main.774fe7e9.chunk.js"
+  },
+  {
+    "revision": "88d10c5c8ce869790a7f",
+    "url": "/static/js/runtime-main.1b659507.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
